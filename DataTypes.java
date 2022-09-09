34a85aa349f29504b01302c4fae9cd00577c8302
Dominick <domenico.damato@vtsd.com>
2022-09-09 12:26:57 -0400

    Day 2.

---
+++ DataTypes.java
@@ -0,0 +1,9 @@
+public class DataTypes {
+    public static void main(String args){
+        // Variable syntax: dataType varName = value;
+        int domFavInt = 5;
+        double snicker = 7.5;
+        boolean likeDogs = true;
+
+    }
+}
